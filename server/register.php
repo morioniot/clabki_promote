d2824913520d8e5bd9bece247d9a7737a4aacf5f
Miguel Ramos <miguel9ramos@gmail.com>
2018-02-26 13:31:12 -0500

    Adding email sender to registry process

--- server/register.php
+++ server/register.php
@@ -3,6 +3,8 @@
     ini_set('log_errors', 1);
     ini_set('error_log', 'php-error.log');
     error_reporting(E_ALL);
+
+    require_once(__DIR__.'/vendor/autoload.php');
     require_once(__DIR__.'/dbconnection.php');
 
     $conexionSql = new DatabaseConnection();
@@ -28,8 +30,64 @@
 
             if(!$conexionSql->query($query)) {
                 $errorMessage = "Lo sentimos, no pudimos almacenar tus datos";
-                //$errorMessage = 'No se pudieron almacenar los datos: '.$conexionSql->error;
+                $errorLogMessage = 'No se pudieron almacenar los datos: '.$conexionSql->error;
+                error_log($errorLogMessage);
                 //throw new Exception($errorMessage, $conexionSql->errno);
+            }
+
+            else {
+
+                //Enviando Email
+
+                $mail = new PHPMailerOAuth;
+
+                $mail->isSMTP();
+                $mail->SMTPDebug = 0;
+                $mail->Debugoutput = 'html';
+                $mail->Host = 'smtp.gmail.com';
+                $mail->Port = 587;
+                $mail->SMTPSecure = 'tls';
+                $mail->SMTPAuth = true;
+                $mail->AuthType = 'XOAUTH2';
+                $mail->oauthUserEmail = 'comunidad.clabki@gmail.com';
+                $mail->oauthClientId = '406616329011-nlr71cg1il8uskgiasc2cvmg7ddgep5k.apps.googleusercontent.com';
+                $mail->oauthClientSecret = '1KF8P9KBLyvfaLZCiEDbTwKk';
+                $mail->oauthRefreshToken = '1/LOG8Ox0nYB1EyLyl_VBlfnwp_jCiahdWjeusIKuEXM8';
+
+                $mail->setFrom('comunidad.clabki@gmail.com', 'Clabki');
+                $mail->addAddress('comunidad.clabki@gmail.com');
+
+                $mail->isHTML( true );
+                $mail->CharSet = 'UTF-8';
+                $mail->Subject = 'Nuevo registro en el formulario - ' . $referenceCode;
+                $mail->Body = '<!DOCTYPE html>
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                        <title>Clabki | Nuevo Registro</title>
+                    </head>
+                    <style media="screen">
+                    </style>
+                    <body>
+                        <section>
+                            <p>
+                                Se ha realizado un nuevo registro a través del formulario en Clabki:
+                            </p>
+                            <ul>
+                                <li><strong>Nombre:</strong> <span>'.$buyerFullName.'</span></li>
+                                <li><strong>Email:</strong> <span>'.$buyerEmail.'</span></li>
+                                <li><strong>Teléfono:</strong> <span>'.$telephone.'</span></li>
+                                <li><strong>Ciudad:</strong> <span>'.$city.'</span></li>
+                                <li><strong>Código de referencia:</strong> <span>'.$referenceCode.'</span></li>
+                            </ul>
+                        </section>
+                    </body>
+                </html>';
+
+                if(!$mail->send()) {
+                    $mensajeError = 'Mailer Error: ' . $mail->ErrorInfo;
+                    error_log($mensajeError);
+                }
             }
     }
     else {
