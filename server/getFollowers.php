cee5eeb0d091fa24a3809e0d50f3a7f174c6231f
Miguel Ramos <miguel9ramos@gmail.com>
2019-11-08 15:55:00 -0500

    Replacing method to get followers from facebook, now it has to be updated manually. Size of the followers count was also updated

--- server/getFollowers.php
+++ server/getFollowers.php
@@ -1,13 +1,13 @@
 <?php
     function getPageFollowers() {
 
-        $fanPageId = '516607761870650';
+        /*$fanPageId = '516607761870650';
         $accessToken = 'access_token=207271499718170|7ede4633f6e471307ce8a678a74922c0';
         $fields = 'fields=fan_count';
         $address = 'https://graph.facebook.com/';
         $url = $address.$fanPageId.'?'.$accessToken.'&'.$fields;
-        $response = file_get_contents( $url );
-        echo $response;
+        $response = file_get_contents( $url );*/
+        echo '{"fan_count": 10500}';
     }
 
     getPageFollowers();
